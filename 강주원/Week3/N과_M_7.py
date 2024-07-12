148bb042846d053c966a44d1885af94145d1beba
강주원 <wizuded@naver.com>
2024-07-12 21:08:42 +0900

    Algo Week3.Fri

--- 강주원/Week3/N과_M_7.py
+++ 강주원/Week3/N과_M_7.py
@@ -2,15 +2,14 @@ n, m = map(int, input().split())
 ls = sorted(list(map(int, input().split())))
 res = []
 
-def back(depth):
+def back():
     if len(res) == m:
         print(' '.join(map(str, res)))
         return
     
     for i in range(n):
         res.append(ls[i])
-        back(depth+1)
+        back()
         res.pop()
 
-
-back(0)
+back()
