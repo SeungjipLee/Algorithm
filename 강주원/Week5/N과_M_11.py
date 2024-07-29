6da863de66ecd58c700fd78f16cbb0bdb4d88269
KJW0821 <force1394@gmail.com>
2024-07-29 19:26:21 +0900

    Algo Week6.Mon

--- 강주원/Week5/N과_M_11.py
+++ 강주원/Week5/N과_M_11.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 ls = sorted(list(map(int, input().split())))
-visit = [0] * n
 temp = []
 
 def sol():
     if len(temp) == m:
         print(*temp)
         return
+    
     now = 0
     for i in range(n):
         if now != ls[i]:
@@ -17,10 +17,5 @@ def sol():
             temp.append(now)
             sol()
             temp.pop()
-sol()
-'''
-1 7 9 9
-1
-1 1
 
-'''
+sol()
