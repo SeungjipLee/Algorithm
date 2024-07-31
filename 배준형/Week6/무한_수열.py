7fe31d7cff00d198d54229bf0ce2502314e6103f
배준형 <wnsgud0895@gmail.com>
2024-07-31 21:29:59 +0900

    240731 junhyung fix

--- 배준형/Week6/무한_수열.py
+++ 배준형/Week6/무한_수열.py
@@ -3,19 +3,35 @@
 #  ⌊x⌋는 x를 넘지 않는 가장 큰 정수이다.
 
 # A(N) 은?
-import sys
+
+# 메모리초과
+import sys, math
 def minput(): return map(int, sys.stdin.readline().split())
 
 N, P, Q = minput()
 
 dp = {}
 dp[0] = 1
+dp[1] = 2
 
-for i in range(1, N//min(P, Q)+1):
-    dp[i] = dp[i//P] + dp[i//Q]
+if N != 0:
+    p_ = int(math.log(N, P)) + 1
+    q_ = int(math.log(N, Q)) + 1
 
-if N == 0:
-    print(1)
-else:
-    print(dp[N//P] + dp[N//Q])
+    idxs = []
+    for i in range(p_+1):
+        for j in range(q_+1):
+            if (N // (P**i*Q**j)) < 2:
+                break
+            idxs.append(N//(P**i*Q**j))
+
+    idxs = list(set(idxs))
+    idxs.sort()
+    # print(idxs)
+
+    for idx in idxs:
+        dp[idx] = dp[idx//P] + dp[idx//Q]
+# print(dp)
+
+print(dp[N])
 
