e3d761e341c29d34228328cb6b3f966e795239dd
KJW0821 <force1394@gmail.com>
2024-08-07 20:23:04 +0900

    Algo Week7.Wed

---
+++ 강주원/Week7/알고리즘_수업_너비_우선_탐색_1.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visit = [0] * (n+1)
+def bfs(x):
+    q = deque()
+    q.append(x)
+    visit[x] = 1
+    cnt = 1
+    while q:
+        x = q.popleft()
+        for i in sorted(graph[x]):
+            if not visit[i]:
+                cnt += 1
+                visit[i] = cnt
+                q.append(i)                
+
+bfs(1)
+for i in visit[1:]:
+    print(i)
