c71722540bc74a31f01ee64d06831406c51bd9a2
KJW0821 <force1394@gmail.com>
2024-08-11 20:21:23 +0900

    Algo Week7.Sun

---
+++ 강주원/Week7/DNA_비밀번호.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+s, p = map(int, input().split())
+ls = list(input().rstrip())
+A, C, G, T = map(int, input().split())
+required = {'A': A, 'C': C, 'G': G, 'T': T}
+current = {i:0 for i in "ACGT"}
+res = 0
+
+
+def check():
+    if all(current[char] >= required[char] for char in "ACGT"):
+        return True
+    
+    return False
+
+for i in range(p):
+    current[ls[i]] += 1
+
+if all(current[char] >= required[char] for char in "ACGT"):
+    res += 1
+
+for i in range(p,s):
+    left_idx = ls[i-p]
+    right_idx = ls[i]
+    current[left_idx] -= 1
+    current[right_idx] += 1
+
+    if check():
+        res += 1
+
+print(res)
--- 강주원/Week7/회전_초밥.py
+++ 강주원/Week7/회전_초밥.py
@@ -1,45 +1,34 @@
 import sys
 input = sys.stdin.readline
-
-n, d, k, c = map(int, input().split())
+n,d,k,c = map(int, input().split())
 ls = [int(input()) for _ in range(n)]
 
-# 슬라이딩 윈도우를 위한 준비
-window = [0] * (d + 1)
-current_kinds = 0
-
-# 처음 k개의 초밥을 윈도우에 추가
+window = [0] * (d+1)
+current_kind = 0
 for i in range(k):
     if window[ls[i]] == 0:
-        current_kinds += 1
+        current_kind += 1
     window[ls[i]] += 1
 
-# 쿠폰으로 제공된 초밥이 이미 윈도우에 없으면 추가
 if window[c] == 0:
-    current_kinds += 1
+    current_kind += 1
     window[c] += 1
 
-# 현재까지의 최대 종류 수를 기록
-res = current_kinds
+res = current_kind
+for i in range(1,n):
+    left_idx = ls[i-1]
+    window[left_idx] -= 1
+    if window[left_idx] == 0:
+        current_kind -= 1
+        
+    right_idx = ls[(i+k-1)%n]
+    if window[right_idx] == 0:
+        current_kind += 1
+    window[right_idx] += 1
 
-# 슬라이딩 윈도우 시작
-for i in range(1, n):
-    # 윈도우에서 가장 왼쪽의 초밥 제거
-    left_index = ls[i - 1]
-    window[left_index] -= 1
-    if window[left_index] == 0:
-        current_kinds -= 1
-
-    # 윈도우에 새롭게 들어올 초밥 추가
-    right_index = ls[(i + k - 1) % n]
-    if window[right_index] == 0:
-        current_kinds += 1
-    window[right_index] += 1
-
-    # 쿠폰 초밥이 윈도우에 없으면 종류를 더해줌 
     if window[c] == 0:
-        res = max(res, current_kinds+1)
+        res = max(res, current_kind + 1)
     else:
-        res = max(res, current_kinds)
+        res = max(res, current_kind)
 
 print(res)
