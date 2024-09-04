664082749b64bb68b317e80cddb5d80ab6881071
KJW0821 <force1394@gmail.com>
2024-09-05 04:56:47 +0900

    Algo Week11.Wed

---
+++ 강주원/Week11/격자상의_경로.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+
+n, m, k = map(int, input().split())
+arr = [[0]*m for _ in range(n)]
+ki, kj = n-1, m-1
+for i in range(n):
+    for j in range(m):
+        val = i*m+j+1
+        arr[i][j] = val
+        if val == k:
+            ki, kj = i, j
+
+dp = [[0]*m for _ in range(n)]
+
+dp[0][0] = 1
+for i in range(ki+1):
+    for j in range(kj+1):
+        dp[i][j] = dp[max(i - 1, 0)][j] + dp[i][max(j - 1, 0)]
+
+
+for i in range(ki,n):
+    for j in range(kj,m):
+        if i == ki and j == kj:
+            continue
+
+        r = dp[i-1][j]
+        c = dp[i][j-1]
+        dp[i][j] = r + c
+
+
+
+print(dp[n-1][m-1])
