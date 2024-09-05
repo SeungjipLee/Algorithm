711f0808770e396631c8167cdc2d95a1a4f74449
KJW0821 <force1394@gmail.com>
2024-09-06 00:27:54 +0900

    Algo Week11.Thu

---
+++ 강주원/Week11/가운데를_말해요.py
@@ -0,0 +1,26 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+min_heap = []
+max_heap = []
+for i in range(n):
+    k = int(input())
+    
+    # max_heap 의 크기가 min_heap 보다 크거나 같게 한다
+    if len(min_heap) == len(max_heap):
+        heapq.heappush(max_heap, -k)
+    else:
+        heapq.heappush(min_heap, k)
+    if min_heap:
+        m = min_heap[0]
+    M = max_heap[0]
+    if min_heap and min_heap[0] < -max_heap[0]:
+        heapq.heappush(max_heap, -heapq.heappop(min_heap))
+        heapq.heappush(min_heap, -heapq.heappop(max_heap))
+
+    print(-max_heap[0])
+
+
--- 강주원/Week11/격자상의_경로.py
+++ 강주원/Week11/격자상의_경로.py
@@ -17,7 +17,12 @@ dp = [[0]*m for _ in range(n)]
 dp[0][0] = 1
 for i in range(ki+1):
     for j in range(kj+1):
-        dp[i][j] = dp[max(i - 1, 0)][j] + dp[i][max(j - 1, 0)]
+        if i == 0 and j == 0:
+            continue
+
+        r = dp[i-1][j]
+        c = dp[i][j-1]
+        dp[i][j] = r + c
 
 
 for i in range(ki,n):
@@ -28,7 +33,6 @@ for i in range(ki,n):
         r = dp[i-1][j]
         c = dp[i][j-1]
         dp[i][j] = r + c
-
 
 
 print(dp[n-1][m-1])
