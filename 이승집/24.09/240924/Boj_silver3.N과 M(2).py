eb31eb2c0a880b00f4098cd399eb7bce2d21d8cb
이승집 <edward626kr@naver.com>
2024-09-24 22:55:13 +0900

    240924 Seungjip

---
+++ 이승집/24.09/240924/Boj_silver3.N과 M(2).py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+
+answer = []
+visited = [0] * (N + 1)
+
+
+def backtracking(arr):
+    global N, M
+    if len(answer) == M:
+        print(*answer)
+        return
+
+    for i in range(1, N + 1):
+        if arr[i] == 0:
+            if answer and answer[-1] >= i:
+                continue
+            answer.append(i)
+            arr[i] = 1
+            backtracking(arr)
+            arr[i] = 0
+            answer.pop()
+
+
+backtracking(visited)
