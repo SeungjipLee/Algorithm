d048719d042cd07823ed8fff6cdb30e4107ebff4
이승집 <edward626kr@naver.com>
2024-10-24 19:53:53 +0900

    241024 Seungjip

---
+++ 이승집/24.10/241024/Pro_Lv3.정수 삼각형.py
@@ -0,0 +1,17 @@
+def solution(triangle):
+    answer = 0
+    N = len(triangle)
+
+    for i in range(1, N):
+        for j in range(i + 1):
+            mid = []
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    return max(triangle[N - 1])
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
