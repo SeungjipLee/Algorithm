36683f007c1c8303a7ef2c0312f650f093e46e95
SeungjipLee <edward@naver.com>
2025-04-25 11:40:29 +0900

    250425 Seungjip

---
+++ 이승집/25.04/250425/Boj1330/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(input);
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
